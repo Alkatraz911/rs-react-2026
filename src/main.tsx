f2fe7fa3df088c246be2bfa448ccc0b590baf7f8
spv <hollysaf@yahoo.com>
2026-05-22 10:17:58 +0300

    feat: added theme context and toggle theme button

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx'
+import { ThemeProvider } from './context/ThemeContext';
 // import './App.css'
 import './styles/main.css';
 import './styles/layout.css';
@@ -12,15 +13,18 @@ import './styles/pagination.css';
 import './styles/navbar.css';
 import './styles/notFoundPage.css';
 import './styles/aboutPage.css';
+import './styles/theme.css'
 
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
